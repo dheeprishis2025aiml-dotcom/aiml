8900bc25a166aa1b8ffe5d93bc121aca08ab58fa
dheeprishis2025aiml-dotcom <dheeprishi.s.2025.aiml@rajalakshmi.edu.in>
2026-08-01 21:18:49 +0530

    Update fo_po.py

--- category2/fo_po.py
+++ category2/fo_po.py
@@ -1,1 +1,17 @@
+# Fully Observable vs Partially Observable
 
+class Environment:
+    def __init__(self):
+        self.rooms = {"A": "Clean", "B": "Dirty"}
+
+    def fully_observable(self):
+        print("Fully Observable Environment")
+        print(self.rooms)
+
+    def partially_observable(self):
+        print("Partially Observable Environment")
+        print({"A": self.rooms["A"]})
+
+env = Environment()
+env.fully_observable()
+env.partially_observable()
