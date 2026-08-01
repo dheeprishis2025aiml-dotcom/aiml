ce421b5e3f003d7cf74d8aa5e6b1f4d813516552
dheeprishis2025aiml-dotcom <dheeprishi.s.2025.aiml@rajalakshmi.edu.in>
2026-08-01 21:23:35 +0530

    Create episodic_sequential.py

---
+++ category2/episodic_sequential.py
@@ -0,0 +1,13 @@
+# Episodic vs Sequential Environment
+
+print("Episodic Environment")
+tasks = ["Task 1", "Task 2", "Task 3"]
+
+for task in tasks:
+    print(task, "completed independently")
+
+print("\nSequential Environment")
+steps = ["Start", "Process", "Finish"]
+
+for step in steps:
+    print(step)
