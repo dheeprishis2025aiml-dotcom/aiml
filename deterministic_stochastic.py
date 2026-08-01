3e5744bf6860e953f1ef95c1b13afab2acec0378
dheeprishis2025aiml-dotcom <dheeprishi.s.2025.aiml@rajalakshmi.edu.in>
2026-08-01 21:22:27 +0530

    Delete deterministic_stochastic.py

--- deterministic_stochastic.py
+++
@@ -1,9 +0,0 @@
-# Deterministic vs Stochastic Environment
-
-import random
-
-print("Deterministic")
-print("2 + 3 =", 2 + 3)
-
-print("\nStochastic")
-print("Random Number:", random.randint(1, 10))
